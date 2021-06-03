4c1e5c385aa191a70df7f2825377f577a091beba
Hahn Jiho <ferrarijm@naver.com>
2021-06-03 19:21:12 +0900

    feat: Front-Simulate local emission

--- rxjs-app/src/App.js
+++ rxjs-app/src/App.js
@@ -1,8 +1,30 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import {from, interval, zip} from 'rxjs'
+import Header from './components/Header'
+import Body from './components/Body'
 
 function App() {
+  const [buf, setBuf] = useState(0)
+  
+  const nums = from([1, 2, 3, 4, 5])
+  const inter = interval(500)
+  const numsInterval = zip(nums, inter)
+
+  useEffect(()=>{
+    numsInterval.subscribe({
+      next: vals=>{
+          console.log("n:", vals[0])
+          setBuf(vals[0])
+      },
+      error: err=>console.log("error:",err),
+      // complete: ()=>console.log("completed"))
+    })
+  })
+
   return (
     <div className="App">
+      <Header/>
+      <Body buf={buf} setBuf={setBuf}/>
     </div>
   );
 }
---
+++ rxjs-app/src/components/Body.js
@@ -0,0 +1,10 @@
+
+const Body = (props) => {
+    return (
+        <div>
+            <p>observed: {props.buf}</p>
+        </div>
+    )
+}
+
+export default Body
