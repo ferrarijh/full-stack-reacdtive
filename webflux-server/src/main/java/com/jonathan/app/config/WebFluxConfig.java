da2602476309d247a44e2b60f3f36abfb41b5032
Hahn Jiho <ferrarijm@naver.com>
2021-06-18 09:59:39 +0900

    Fixed #5 - DataBufferLimitException triggered while saving certain images
    Solved by increasing the buffer size of webclient codec while creating WebClient instance.
    
    ```java
        @Bean
        public WebClient webClient(){
            return WebClient.create().mutate().codecs(
                    configurer -> configurer.defaultCodecs().maxInMemorySize(10*1024*1024)
            ).build();
        }
    ```

---
+++ webflux-server/src/main/java/com/jonathan/app/config/WebFluxConfig.java
@@ -0,0 +1,13 @@
+package com.jonathan.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class WebFluxConfig implements WebFluxConfigurer {
+    @Override
+    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+        configurer.defaultCodecs().maxInMemorySize(10*1024*1024);
+    }
+}
--- webflux-server/src/main/java/com/jonathan/app/reactiveclient/ReactiveClient.java
+++ webflux-server/src/main/java/com/jonathan/app/reactiveclient/ReactiveClient.java
@@ -2,6 +2,7 @@ package com.jonathan.app.reactiveclient;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
@@ -9,6 +10,8 @@ public class ReactiveClient {
 
     @Bean
     public WebClient webClient(){
-        return WebClient.create();
+        return WebClient.create().mutate().codecs(
+                configurer -> configurer.defaultCodecs().maxInMemorySize(10*1024*1024)
+        ).build();
     }
 }
